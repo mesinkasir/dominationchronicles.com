2caf3dff06256fefbda1b04dda14173962c92760
adamdjbrett <adamdjbrett@users.noreply.github.com>
2025-11-26 16:40:09 -0600

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -32,7 +32,7 @@ eleventyConfig.addPlugin(eleventyPluginYoutubeEmbed);
 			"./public/": "/",
 			"./public/img/favicon/favicon.ico": "/favicon.ico"
 		})
-		// Copy xmit.json to output root for Xmit hosting
+		// Copy xmit.json (JSON) to output root for Xmit hosting
 		.addPassthroughCopy({ "./xmit.json": "/xmit.json" })
 		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl")
 		.addPassthroughCopy("./public/img/favicons")
